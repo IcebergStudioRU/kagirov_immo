23454166102de0ef3392ce19a37c1a9f5772d5cd
Muhammad9500 <84620962+Muhammad9500@users.noreply.github.com>
2022-08-13 19:53:20 +0300

    added changes

--- src/components/pages/main/aboutMe/AboutMe.js
+++ src/components/pages/main/aboutMe/AboutMe.js
@@ -3,11 +3,16 @@ import {ReactComponent as Stripes} from "./svg/svg-stripes.svg"
 import Zelimhan from "./images/zelimkhan.png"
 const AboutMe = () => {
     return (
-        <div className="bg-aboutMeBg w-1/1 h-550 pt-12" >
-            <div className="bg-aboutMeImage bg-no-repeat bg-cover">
-            <div>
+        <div className="bg-aboutMeBg w-1/1 h-550 pt-4" >
+
+            <div className='flex justify-center'>
                 <img src={Zelimhan}/>
             </div>
+            <div className='p-4 flex flex-col items-center '>
+                <h1 className='text-white mb-8'>AboutMe</h1>
+                <p className='text-center text-white'>Lorem ipsum dolor sit amet, consectetur<br/> adipiscing elit. Suspendisse varius enim in eros<br/> elementum tristique. </p>
+
+<p className='text-center text-white'>Duis cursus, mi quis viverra ornare, eros dolor<br/> interdum nulla, ut commodo diam libero vitae<br/> erat. Aenean faucibus nibh et justo cursus id<br/> rutrum lorem imperdiet</p>
             </div>
         </div>
     );
