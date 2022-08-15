2f08ec7613422233d667a88f64c8227265bfee2e
Muhammad9500 <84620962+Muhammad9500@users.noreply.github.com>
2022-08-15 11:49:50 +0300

    added changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
+import './Header.css'
 import { ReactComponent as Logo } from "./svg/logo-kagirov-immo.svg";
 import Hamburger from "hamburger-react";
 const Header = ({mobileMenu, setMobileMenu}) => {
+  const body = document.querySelector('body')
+  const overflowBody = mobileMenu ? "scroll" : "hidden";
   const changeMobileMenu = () => {
     setMobileMenu((prev) => !prev)
+    body.style.overflow = `${overflowBody}`
+    body.style.overflowX = "hidden"
   }
   return (
-    <div className="header bg-headerBg">
+    <div className={mobileMenu ? 'headerFixed' : 'headerDefault'}>
       <div className="max-w-mobileContainer mx-auto p-5 flex justify-between ">
         <Logo />
         <Hamburger
